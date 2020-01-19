b94b0e25ff87f3fdf5cfe4c7720036e02f4654f8
Idiot_AG <maheshkrishnagopal@gmail.com>
2020-01-19 09:22:57 +0530

    updated with single function execution.

--- transfer_to_db.py
+++ transfer_to_db.py
@@ -3,29 +3,41 @@ Name: Database Loader Script
 Author: Maheshkrishna A G
 Description: The script access the task_data.csv file and read them to load into PYTHON_TEST.TASK_DATA table.
 Database: MySQL 8.0.16 in AWS RDS
-Date: 18-Jan-2020
+Date: 19-Jan-2020
 """
 
 import pandas as pd
 import mysql.connector as mysql
 import time
 
-df = pd.read_csv("PATH TO task_data.csv")
 
-# Initiate DB Connection
-db = mysql.connect(host="python-db.cxxjqwuxxbfj.eu-west-1.rds.amazonaws.com", user="user_one",
-                   passwd=base64.b64decode(b'TWFzdGVyIzEyMw=='.decode("utf-8")).decode("utf-8"),
-                   db="python_test")
-cur = db.cursor()
+def transfer_data():
+    """
+    transfer_data function will read the task_data.csv file and load it into the MySQL database table,
+    PYTHON_TEST.TASK_DATA.
+    :return: None
+    """
+    df = pd.read_csv("task_data.csv")
 
-# Traverse through each data frame to create a record in the PYTHON_TEST.TASK_DATA table
-for i in df.index:
-    timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.strptime(str(df['timestamp'][i]), "%m/%d/%Y %H:%M:%S"))
-    try:
-        cur.execute("INSERT INTO TASK_DATA VALUES (%s,%s,%s,%s)", (int(df['id'][i]), str(timestamp),
-                                                                   float(df['temperature'][i]), str(df['duration'][i])))
-    except Exception as e:
-        print(e)
-    db.commit()
+    # Initiate DB Connection
+    db = mysql.connect(host="python-db.cxxjqwuxxbfj.eu-west-1.rds.amazonaws.com", user="user_one",
+                       passwd=base64.b64decode(b'TWFzdGVyIzEyMw=='.decode("utf-8")).decode("utf-8"),
+                       db="python_test")
+    cur = db.cursor()
 
-db.close()
+    # Traverse through each data frame to create a record in the PYTHON_TEST.TASK_DATA table
+    for i in df.index:
+        timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.strptime(str(df['timestamp'][i]), "%m/%d/%Y %H:%M:%S"))
+        try:
+            cur.execute("INSERT INTO TASK_DATA VALUES (%s,%s,%s,%s)", (int(df['id'][i]), str(timestamp),
+                                                                       float(df['temperature'][i]),
+                                                                       str(df['duration'][i])))
+        except Exception as e:
+            print(e)
+        db.commit()
+
+    db.close()
+
+
+if __name__ == "__main__":
+    transfer_data()
